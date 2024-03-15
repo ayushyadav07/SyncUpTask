e4a1716d5b82ce0c2e038bfa421fcebad015fd13
ayushyadav07 <105917298+ayushyadav07@users.noreply.github.com>
2024-03-15 11:58:15 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,453 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SyncUp Development',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => con(),
+        '/chat': (con) => chat(),
+        '/profile': (con) => ps(),
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class con extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _conState createState() => _conState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _conState extends State<con> {
+  List<bool> ls = List.filled(7, false);
+  List<bool> bs = List.filled(7, false);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  List<String> profileNames = [
+    'username1',
+    'username2',
+    'username3',
+    'username4',
+    'username5',
+    'username6',
+    'username7',
+  ];
+
+  @override
+  Widget build(BuildContext con) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Instagram',
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.live_tv),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.send),
+            onPressed: () {
+              Navigator.pushNamed(con, '/chat');
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 7,
+        itemBuilder: (context, index) {
+          List<String> imageUrls = [
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBZRzTKK-EHfAy-6YKNs0ggsvY_io3CfT7QC-P4az-hI5vUpGC',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFWI73wrE-fWmGWfw4InXCD3TAyLYBYCd97t-RCMPD_z0yf5yp',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQQwgfWL1PcYDRbCnZivZqcQ2rMjkpTIWm7dFcBE-ILEini2wC2',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSmn7LsTUOyDqUD4i3xFSk5JW5rqhBkYwAKwdoe4xbPRqQ80ed1',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQSERiM8xvOx_mPLjl7RhZevemuR9MAV0zLxvJecRkkOAk4Ndl9',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTAQANFE0jokbbX-kfwGhrRRrReqP28AAspKXFnC7E2tTuVnJzA',
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSeICY8jNEC8JFvekFzcC4SB1684MvoINp7b82pH5MXN8dc2ksK ',
+          ];
+
+          List<String> captions = [
+            'Gareth Bale\'s stunning overhead kick secured a 3-1 victory against Liverpool!',
+            'Messi celebrates fourth goal in Champions League quarterfinal for Barcelona!',
+            'Anthony Gordon\'s second-half goal halts Gunners\' unbeaten Premier League start!',
+            'Gareth Bale confirmed he is leaving Real Madrid on Wednesday!',
+            'Real Madrid 2 - 3 Barça (2017) Messi grabs dramatic late win in #ElClásico!!',
+            'UEFA Champions League officially kicked off from 19 September 2023!',
+            'Tottenham\'s Harry Kane Sets Record for Most Goals for Single Team in EPL History!',
+          ];
+
+          String imageUrl = imageUrls[index % imageUrls.length];
+          String caption = captions[index % captions.length];
+          String profileName = profileNames[index % profileNames.length];
+
+          return PostItem(
+            imageUrl: imageUrl,
+            caption: caption,
+            isLiked: ls[index],
+            isBookmarked: bs[index],
+            onLiked: () {
+              setState(() {
+                ls[index] = !ls[index];
+              });
+            },
+            onBookmarked: () {
+              setState(() {
+                bs[index] = !bs[index];
+              });
+            },
+            profileName: profileName,
+          );
+        },
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: Icon(Icons.home),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.add_box),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.account_circle),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/profile');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+}
+
+class PostItem extends StatelessWidget {
+  final String imageUrl;
+  final String caption;
+  final bool isLiked;
+  final bool isBookmarked;
+  final VoidCallback onLiked;
+  final VoidCallback onBookmarked;
+  final String profileName;
+
+  PostItem({
+    required this.imageUrl,
+    required this.caption,
+    required this.isLiked,
+    required this.isBookmarked,
+    required this.onLiked,
+    required this.onBookmarked,
+    required this.profileName,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              CircleAvatar(
+                radius: 16,
+                backgroundImage: NetworkImage(
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHE4vcD6O1-GAxEU2CDLEQlD140pQI94q5qA&usqp=CAU',
+                ),
+              ),
+              SizedBox(width: 8),
+              Text(
+                profileName,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+          height: 300,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                icon: isLiked ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
+                color: isLiked ? Colors.red : null,
+                onPressed: onLiked,
+              ),
+              IconButton(
+                icon: Icon(Icons.chat_bubble_outline),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.share),
+                onPressed: () {},
+              ),
+              Spacer(),
+              IconButton(
+                icon: isBookmarked ? Icon(Icons.bookmark) : Icon(Icons.bookmark_border),
+                onPressed: onBookmarked,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            caption,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class chat extends StatefulWidget {
+  @override
+  _chatState createState() => _chatState();
+}
+
+class _chatState extends State<chat> {
+  final TextEditingController _messageController = TextEditingController();
+  List<String> _messages = [];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Chats'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                return MessageItem(_messages[index]);
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _messageController,
+                    decoration: InputDecoration(
+                      hintText: 'Message...',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    if (_messageController.text.isNotEmpty) {
+                      sendMessage(_messageController.text);
+                      _messageController.clear();
+                    }
+                  },
+                ),
+              ],
             ),
-          ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  void sendMessage(String message) {
+    setState(() {
+      _messages.add(message);
+      _messages.add('Thanks for your message!');
+    });
+  }
+}
+
+class MessageItem extends StatelessWidget {
+  final String message;
+
+  MessageItem(this.message);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: Colors.grey.shade200,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Text(message),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ps extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Username'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: NetworkImage(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHE4vcD6O1-GAxEU2CDLEQlD140pQI94q5qA&usqp=CAU'),
+              ),
+              SizedBox(width: 20),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'ayussssshhhhhhh',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            OutlinedButton(
+                              onPressed: () {
+                              },
+                              child: Text('Edit Profile'),
+                            ),
+                            SizedBox(width: 10),
+                            OutlinedButton(
+                              onPressed: () {
+                              },
+                              child: Text('View Archive'),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              '0',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(' posts'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              '391',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(' followers'),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              '441',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(' following'),
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Ayush Yadav\n'
+                      'IITK\'25\n'
+                      'Cricket❤️ || Chess❤️\n'
+                      'The comeback is always stronger than the setback.🔥🔥\n',
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(height: 20),
+                    
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
